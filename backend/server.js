315fa149044cfff3bd54485edd035c7f1a35fb7c
StvnMndz <aidansteven84@gmail.com>
2026-09-22 00:09:15 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const empleadosRoutes = require('./routes/empleados');
+const insumosRoutes = require('./routes/insumos');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.get('/api/health', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/empleados', empleadosRoutes);
+app.use('/api/insumos', insumosRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
